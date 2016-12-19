db80dd577997cf0eaf5686af7fc7fff095560d70
baker_wang <baker_wang@wistron.com>
2016-12-19 09:45:13 +0800

    Initial Commit

---
+++ TEST/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TEST
+//
+//  Created by MAC on 2016/12/19.
+//  Copyright © 2016年 Wistron. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ TEST/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  TEST
+//
+//  Created by MAC on 2016/12/19.
+//  Copyright © 2016年 Wistron. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
