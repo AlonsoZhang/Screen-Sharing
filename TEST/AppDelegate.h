28d481058aa3646ab29c5066d445addef146f9cb
Alonso Zhang <code@zhangwu.space>
2017-03-23 11:12:55 +0800

    Modify

--- TEST/AppDelegate.h
+++ TEST/AppDelegate.h
@@ -7,19 +7,21 @@
 //
 
 #import <Cocoa/Cocoa.h>
+#import "JFHotkeyManager.h"
 
 @interface AppDelegate : NSObject <NSApplicationDelegate>
 {
-    NSDictionary *ConfigPlist;
-    NSString     *DefaultType;
-    
+    NSDictionary    *ConfigPlist;
+    JFHotkeyManager *hkm;
 }
+
+@property  BOOL SHOW;
 @property  NSMutableArray *Users;
-
-
 @property (weak) IBOutlet NSTableView *TABLE;
 @property(readwrite, retain) NSStatusItem *statusItem;
 
+@property (weak) IBOutlet NSPanel *Window;
+- (IBAction)create:(NSButton *)sender;
 
 @end
 
--- TEST/AppDelegate.m
+++ TEST/AppDelegate.m
@@ -10,13 +10,14 @@
 
 @interface AppDelegate ()
 
-@property (weak) IBOutlet NSWindow *window;
 @end
 
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
+    self.SHOW = YES;
+    
     //******************  Inin Config ******************
     ConfigPlist = [[NSDictionary alloc] initWithContentsOfFile:[[NSBundle mainBundle]pathForResource:@"Config" ofType:@"plist"]];
     
@@ -29,6 +30,14 @@
     
     //******************  set StatusBarItem  ******************
     [self StatusBarItem];
+    
+    [self.Window setTitle:[NSString stringWithFormat:@"%@ --(option+A)",[ConfigPlist objectForKey:@"DefaultType"]]];
+    
+    hkm = [[JFHotkeyManager alloc] init];
+
+    [hkm bind:@"option a" target:self action:@selector(HotKeyChangeType)];
+    [hkm bind:@"option z" target:self action:@selector(HIDESHOW)];
+    //[hkm bind:@"option s" target:self action:@selector(password)];
 }
 
 
@@ -37,17 +46,20 @@
 }
 
 
--(BOOL)applicationShouldTerminateAfterLastWindowClosed:(NSApplication *)sender
-{
-    [self.Users writeToFile:[[NSBundle mainBundle]pathForResource:[ConfigPlist objectForKey:@"DefaultType"] ofType:@"plist"] atomically:YES];
-    return YES;
-}
+//-(BOOL)applicationShouldTerminateAfterLastWindowClosed:(NSApplication *)sender
+//{
+//    [self.Users writeToFile:[[NSBundle mainBundle]pathForResource:[ConfigPlist objectForKey:@"DefaultType"] ofType:@"plist"] atomically:YES];
+//    return YES;
+//}
 
 - (void)Doopen{
     NSString *ip =[[self.Users objectAtIndex:self.TABLE.selectedRow] objectForKey:@"IP"];
     if([ip length]>0)
     {
-        [self RunCMD:[NSString stringWithFormat:@"do shell script \"Open vnc://%@\" ",ip]];
+        NSString *cmd =[NSString stringWithFormat:@"do shell script \"Open vnc://%@\" ",ip];
+        [self RunCMD:cmd];
+        
+        [self password];
     }
 }
 
@@ -61,9 +73,13 @@
 
 - (void)password
 {
-  [self RunCMD:[NSString stringWithFormat:[ConfigPlist objectForKey:@"InputPswScript"],[[self.Users objectAtIndex:self.TABLE.selectedRow] objectForKey:@"Username"],[[self.Users objectAtIndex:self.TABLE.selectedRow] objectForKey:@"Password"]]];
-    
-    NSLog(@"%@", [NSString stringWithFormat:[ConfigPlist objectForKey:@"InputPswScript"],[[self.Users objectAtIndex:self.TABLE.selectedRow] objectForKey:@"Username"],[[self.Users objectAtIndex:self.TABLE.selectedRow] objectForKey:@"Password"]]);
+    [NSThread sleepForTimeInterval:1];
+    NSString * cmd = [NSString stringWithFormat:[ConfigPlist objectForKey:@"InputPswScript"],[[self.Users objectAtIndex:self.TABLE.selectedRow] objectForKey:@"Username"],[[self.Users objectAtIndex:self.TABLE.selectedRow] objectForKey:@"Password"]];
+
+    //NSString * cmd = [NSString stringWithFormat:@"tell application \"Screen Sharing\"\nactivate\ntell application \"System Events\"\nkeystroke \"%@\"\nkeystroke tab\nkeystroke \"%@\"\nkeystroke tab\nend tell\nend tell\n",@"gdlocal",@"gdlocal"];
+    //[self RunCMD:[NSString stringWithFormat:[ConfigPlist objectForKey:@"InputPswScript"],[[self.Users objectAtIndex:self.TABLE.selectedRow] objectForKey:@"Username"],[[self.Users objectAtIndex:self.TABLE.selectedRow] objectForKey:@"Password"]]];
+    //NSLog(@"%@", cmd);
+    [self RunCMD:cmd];
 }
 
 - (void)StatusBarItem {
@@ -71,10 +87,11 @@
     self.statusItem= [[NSStatusBar systemStatusBar] statusItemWithLength:NSVariableStatusItemLength];
     
     NSMenu *MainMenu = [[NSMenu alloc]init];
-    NSMenuItem *Password = [[NSMenuItem alloc] initWithTitle:@"Input_Password" action:@selector(password) keyEquivalent:@""];
+    //NSMenuItem *Password = [[NSMenuItem alloc] initWithTitle:@"Input_Password" action:@selector(password) keyEquivalent:@""];
+    NSMenuItem *Hide = [[NSMenuItem alloc] initWithTitle:@"Hide/Show" action:@selector(HIDESHOW) keyEquivalent:@""];
     NSMenuItem *Quit = [[NSMenuItem alloc] initWithTitle:@"Quit" action:@selector(quit) keyEquivalent:@""];
-    [MainMenu addItem:Password];
-    
+    //[MainMenu addItem:Password];
+    [MainMenu addItem:Hide];
     
     NSArray *Type = [ConfigPlist objectForKey:@"Type"];
     for( NSString *Name in Type )
@@ -105,6 +122,53 @@
     
     //******************  Save DefaultType ******************
     [ConfigPlist writeToFile:[[NSBundle mainBundle]pathForResource:@"Config" ofType:@"plist"] atomically:YES];
+    [self.Window setTitle:[NSString stringWithFormat:@"%@ --(option+A)",[ConfigPlist objectForKey:@"DefaultType"]]];
+}
+
+-(void) HIDESHOW
+{
+    if (self.SHOW)
+    {
+        self.SHOW = NO;
+    }
+    else
+    {
+        self.SHOW =YES;
+    }
+}
+
+-(void) HotKeyChangeType
+{
+    self.SHOW =YES;
+    
+    //******************  Save Users firstly ******************
+    [self.Users writeToFile:[[NSBundle mainBundle]pathForResource:[ConfigPlist objectForKey:@"DefaultType"] ofType:@"plist"] atomically:YES];
+    
+    //******************  Chage self.Users ******************
+    NSArray* TypeArry = [ConfigPlist objectForKey:@"Type"];
+
+    for (int i=0; i < TypeArry.count; i++ )
+    {
+        if ([[TypeArry objectAtIndex:i] isEqualToString:[ConfigPlist objectForKey:@"DefaultType"]])
+        {
+            i++;
+            if (i == TypeArry.count)
+            {
+                i=0;
+            }
+            
+            //Chage DefaultType
+            [ConfigPlist setValue:[TypeArry objectAtIndex:i] forKey:@"DefaultType"];
+            //******************  Save DefaultType ******************
+            [ConfigPlist writeToFile:[[NSBundle mainBundle]pathForResource:@"Config" ofType:@"plist"] atomically:YES];
+
+        }
+    }
+    
+    //******************  Chage self.Users ******************
+    NSMutableArray *ArryFromPlist = [[NSMutableArray alloc] initWithContentsOfFile:[[NSBundle mainBundle]pathForResource:[ConfigPlist objectForKey:@"DefaultType"] ofType:@"plist"]];
+    self.Users=([ArryFromPlist count]>0)?ArryFromPlist:[NSMutableArray new];
+    [self.Window setTitle:[NSString stringWithFormat:@"%@ --(option+A)",[ConfigPlist objectForKey:@"DefaultType"]]];
 }
 
 
@@ -112,5 +176,8 @@
 {
     [self.Users writeToFile:[[NSBundle mainBundle]pathForResource:[ConfigPlist objectForKey:@"DefaultType"] ofType:@"plist"] atomically:YES];
     [NSApp terminate:self];
+}
+- (IBAction)create:(NSButton *)sender {
+    //[[self.Users objectAtIndex:self.TABLE.selectedRow]setObject:@"gdlocal" forKey:@"IP"];
 }
 @end
